a6223c5d45bf21fd1f560d4617dcc8f2a1910c4d
amb <mikaelh1651@gmail.com>
2023-02-17 22:56:41 +0900

    feat(cam_screen): agora api 및 event handler 작성

--- lib/screen/cam_screen.dart
+++ lib/screen/cam_screen.dart
@@ -1,5 +1,8 @@
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+
+import '../const/agora.dart';
 
 class CamScreen extends StatefulWidget {
   const CamScreen({super.key});
@@ -9,6 +12,10 @@ class CamScreen extends StatefulWidget {
 }
 
 class _CamScreenState extends State<CamScreen> {
+  RtcEngine? engine;
+  int? uid;
+  int? otherUid;
+
   Future<bool> init() async {
     final response = await [Permission.camera, Permission.microphone].request();
 
@@ -18,6 +25,57 @@ class _CamScreenState extends State<CamScreen> {
     if (cameraPermission != PermissionStatus.granted ||
         micPermission != PermissionStatus.granted) {
       throw '카메라 또는 마이크 권한이 없습니다.';
+    }
+
+    if (engine == null) {
+      engine = createAgoraRtcEngine();
+
+      await engine!.initialize(
+        RtcEngineContext(
+          appId: APP_ID,
+          channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
+        ),
+      );
+
+      engine!.registerEventHandler(
+        RtcEngineEventHandler(
+          onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
+            print('채널에 입장했습니다. uid : ${connection.localUid}');
+            setState(() {
+              uid = connection.localUid;
+            });
+          },
+          onLeaveChannel: (RtcConnection connection, RtcStats stats) {
+            print('채널 퇴장');
+            setState(() {
+              uid = null;
+            });
+          },
+          onUserJoined: (RtcConnection connection, int remoteUid, int elapsed) {
+            print('상대가 채널에 입장했습니다. uid : $remoteUid');
+            setState(() {
+              otherUid = remoteUid;
+            });
+          },
+          onUserOffline: (RtcConnection connection, int remoteUid,
+              UserOfflineReasonType reason) {
+            print('상대가 채널에서 나갔습니다. uid : $otherUid');
+            setState(() {
+              otherUid = null;
+            });
+          },
+        ),
+      );
+
+      await engine!.setClientRole(role: ClientRoleType.clientRoleBroadcaster);
+      await engine!.enableVideo();
+      await engine!.startPreview();
+      await engine!.joinChannel(
+        token: TEMP_TOKEN,
+        channelId: CANNEL_NAME,
+        uid: 0,
+        options: ChannelMediaOptions(),
+      );
     }
 
     return true;
