9dd97fc130b80ff719d3d3eec725031704ee3f36
amb <mikaelh1651@gmail.com>
2023-02-17 21:31:19 +0900

    feat(cam_screen): 카메라, 마이크 권한 받는 기능 작성

--- lib/screen/cam_screen.dart
+++ lib/screen/cam_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class CamScreen extends StatefulWidget {
   const CamScreen({super.key});
@@ -8,14 +9,45 @@ class CamScreen extends StatefulWidget {
 }
 
 class _CamScreenState extends State<CamScreen> {
+  Future<bool> init() async {
+    final response = await [Permission.camera, Permission.microphone].request();
+
+    final cameraPermission = response[Permission.camera];
+    final micPermission = response[Permission.microphone];
+
+    if (cameraPermission != PermissionStatus.granted ||
+        micPermission != PermissionStatus.granted) {
+      throw '카메라 또는 마이크 권한이 없습니다.';
+    }
+
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('LIVE'),
       ),
-      body: Center(
-        child: Text('Cam Screen'),
+      body: FutureBuilder(
+        future: init(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                snapshot.error.toString(),
+              ),
+            );
+          }
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Center(
+            child: Text('모든 권한이 있습니다!'),
+          );
+        },
       ),
     );
   }
